6305e725e2f83dbc45acb698e7ad493809c71fb2
jaba67 <2350483j@umich.mx>
2024-10-04 13:11:25 -0600

    Add files via upload

---
+++ CETES.R
@@ -0,0 +1,10 @@
+# se presentaran las variables para las formulas de los problemas de interes simple.
+# Autor: Jesus Adriel Bravo Aguilar
+
+# Formula de interes simple:
+
+valorfinalSimple=function(VA,r,t){
+  VF=VA*(1+(r*t))
+  
+  return(VF)
+}
